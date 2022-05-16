a0dc15f9d7ef150cad05269ffe8ad5e7a80bab1c
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2022-05-16 17:38:41 -0300

    beginning of Uno-like game

---
+++ app/Models/UnoGame.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UnoGame extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'user_token',
+        'status',
+        'result',
+        'player0',
+        'player0points',
+        'player1',
+        'player1points',
+        'player2',
+        'player2points',
+        'player3',
+        'player3points',
+        'deck',
+        'turns',
+        'date_started'
+    ];
+}
---
+++ database/migrations/2022_05_16_192650_create_uno_games_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uno_games', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('user_token');
+            $table->string('status');
+            $table->string('result');
+            $table->text('player0');
+            $table->integer('player0points');
+            $table->text('player1');
+            $table->integer('player1points');
+            $table->text('player2');
+            $table->integer('player2points');
+            $table->text('player3');
+            $table->integer('player3points');
+            $table->text('deck');
+            $table->integer('turns');
+            $table->timestamp('date_started', $precision=0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uno_games');
+    }
+};
