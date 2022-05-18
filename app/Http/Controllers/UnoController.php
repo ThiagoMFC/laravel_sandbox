c83fe37c24d1c648e80d289c00e71a90a16c2812
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2022-05-18 19:08:47 -0300

    uno npcPlay method not finished

--- app/Http/Controllers/UnoController.php
+++ app/Http/Controllers/UnoController.php
@@ -167,6 +167,10 @@ class UnoController extends Controller
             }
         }
 
+
+
+        
+
         if($cardOnTable[1] == 'reverse'){
             //plays 4 to 0
             $direction = 'counterClockWise';
@@ -175,10 +179,6 @@ class UnoController extends Controller
             $direction = 'clockWise';
             //if($cardOnTable[1] == 'block'){skip player 0}
         }
-
-        //if clockwise -> player plays first
-        //if counterclockwise -> player plays last
-        //first to play can be skipped
 
         $p0h = serialize($player0hand);
         $p1h = serialize($player1hand);
@@ -213,10 +213,14 @@ class UnoController extends Controller
             return response([
                 'message' => 'game started and is running '. $direction,
                 'on table' => $cardOnTable,
-                'player' => $player0hand,
+                'your cards' => $player0hand,
+                'your points' => 0,
                 'player 2 hand' => count($player1hand),
+                'player 2 points' => 0,
                 'player 3 hand' => count($player2hand),
-                'player 4 hand' => count($player3hand)
+                'player 3 points' => 0
+                'player 4 hand' => count($player3hand),
+                'player 4 points' => 0
             ], 201);
         }else{
             return response([
@@ -248,5 +252,129 @@ class UnoController extends Controller
         return response([
             'message' => 'game closed'
         ], 200);
+    }
+
+    public function userPlay(Request $request){
+
+        $fields = $request->validate([
+            'user_id' => 'required',
+        ]);
+
+        $token = $request->bearerToken();
+
+        $helper = new HelperClass();
+        $validateUser = $helper->checkToken($fields['user_id'], $token);
+
+        if(!$validateUser){
+            return response([
+                'message' => 'invalid request, user invalid',
+            ], 401);
+        }
+
+
+    }
+
+    function npcPlay($hand, $deck, $pile, $color){
+
+        //hand, deck and pile are subsets of the same original array so same structure
+        
+        $topPileColor = end($pile)[0];
+        $topPileAction = end($pile)[1];
+
+        //check if special card
+        if($topPileAction == '+4'){
+            //if wild +4 draws 4 and misses turn
+
+            for($i = 0; $i < 4; $i++){
+                array_push($hand, [end($deck)[0], end($deck)[1]]);
+                unset($deck[array_key_last($deck)]);
+            }
+            
+            array_push($pile, [$topPileColor, '-']);
+
+            return [$hand, $deck, $pile];
+
+        }else if($topPileAction == '+2'){
+            //if +2 draws 2 and misses turn
+
+            for($i = 0; $i < 2; $i++){
+                array_push($hand, [end($deck)[0], end($deck)[1]]);
+                unset($deck[array_key_last($deck)]);
+            }
+
+            array_push($pile, [$topPileColor, '-']);
+
+            return [$hand, $deck, $pile];
+        }
+
+        //case user has played wild card and selected color, otherwise it's empty
+        /*if($color != ''){
+            $topPileColor = $color;
+        }*/
+
+        $playableCard = false;
+
+        foreach($hand as $key=>$card){
+            if($card[0] == $topPileColor || $card[1] == $topPileAction){
+                //play one card matching the discard (pile) in color, number, or symbol
+                array_push($pile, $card);
+                unset($hand[$key]);
+                $playableCard = true;
+                break;
+            }else if($card[0] == 'wild' && $card[1] != '+4'){
+                //play a Wild card except wild +4
+                array_push($pile, $card);
+                unset($hand[$key]);
+                $playableCard = true;
+
+                //pick color to be played by next player
+                $colors = ['red', 'green', 'blue', 'yellow'];
+                $color = array_rand(array_flip($colors), 1);
+
+                array_push($pile, [$color, '-']);
+
+                break;
+            }
+        }
+
+        if(!$playableCard){
+
+            $wild4Key = array_search('+4', array_column($hand, 1));
+
+            if($wild4Key != ''){
+                //if wild +4 available on hand, play it
+
+                array_push($pile, $hand[$wild4Key]);
+                unset($hand[$wild4Key]);
+
+                //pick color to be played by next player
+                $colors = ['red', 'green', 'blue', 'yellow'];
+                $color = array_rand(array_flip($colors), 1);
+
+                array_push($pile, [$color, '+4']);
+
+            }else if($topPileColor == end($deck)[0] || $topPileAction == end($deck)[1]){
+                //draw card, play it if possible (color)
+
+                array_push($pile, [end($deck)[0], end($deck)[1]]);
+
+            }else if([end($deck)[0] == 'wild'){
+                //draw card, play it if possible (wild)
+
+                array_push($pile, [end($deck)[0], end($deck)[1]]);
+
+            }else{
+                //draw card, can't play. add to hand
+
+                array_push($hand, [end($deck)[0], end($deck)[1]]);
+
+            }
+
+            unset($deck[array_key_last($deck)]);
+        }
+
+
+        return [$hand, $deck, $pile];
+
     }
 }
